d22b77dbe9aa8266cff24638eb02362d818a55a7
Nathaniel Sayson <nathsay23@gmail.com>
2024-11-15 11:16:43 +0800

    Update background.py

--- background.py
+++ background.py
@@ -4,7 +4,7 @@ def apply_background(image_base64):
     .stApp {{
         background-image: url("data:image/jpg;base64,{image_base64}"); 
         background-size: contain;
-        background-position: center;
+        background-position: right;
         background-repeat: no-repeat;
         background-attachment: fixed;
     }}
@@ -17,14 +17,23 @@ def apply_background(image_base64):
             1px 1px 0 #000; 
         font-size: 32px;
     }}
+    .outlined-text p {{
+        font-size: 32px !important;  /* Force font size change for paragraphs */
+        color: white;
+        text-shadow: 
+            -1px -1px 0 #000,  
+            1px -1px 0 #000,
+            -1px 1px 0 #000,
+            1px 1px 0 #000; 
+    }}
     .outlined-text ul, .outlined-text li {{
+        font-size: 32px !important;  /* Force font size change for list items */
         color: white;
         text-shadow: 
             -1px -1px 0 #000,  
             1px -1px 0 #000,
             -1px 1px 0 #000,
             1px 1px 0 #000;        
-        font-size: 32px !important;
     }}
     </style>
     '''
