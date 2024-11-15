ab70917efa1345cf15ec0baac45df42a3a86bbb1
Nathaniel Sayson <nathsay23@gmail.com>
2024-11-15 19:24:10 +0800

    Update background.py

--- background.py
+++ background.py
@@ -1,3 +1,24 @@
+tint = """
+    <style>
+        .streamlit-expanderHeader {
+            font-size: 16px;
+            color: white;
+        }
+        .dataframe tbody tr th, .dataframe tbody tr td {
+            background-color: rgba(255, 255, 255, 0.3);  /* White background with 30% opacity */
+            color: white;  /* Set text color to white for better contrast */
+        }
+        .dataframe thead {
+            background-color: rgba(255, 255, 255, 0.5);  /* Header background with 50% opacity */
+        }
+        .dataframe {
+            border: 2px solid rgba(255, 255, 255, 0.3); /* Transparent border */
+            border-radius: 10px;
+        }
+    </style>
+"""
+
+
 def apply_background(image_base64):
     return f'''
     <style>
@@ -20,22 +41,3 @@ def apply_background(image_base64):
     </style>
     '''
 
-tint = """
-    <style>
-        .streamlit-expanderHeader {
-            font-size: 16px;
-            color: white;
-        }
-        .dataframe tbody tr th, .dataframe tbody tr td {
-            background-color: rgba(255, 255, 255, 0.3);  /* White background with 30% opacity */
-            color: white;  /* Set text color to white for better contrast */
-        }
-        .dataframe thead {
-            background-color: rgba(255, 255, 255, 0.5);  /* Header background with 50% opacity */
-        }
-        .dataframe {
-            border: 2px solid rgba(255, 255, 255, 0.3); /* Transparent border */
-            border-radius: 10px;
-        }
-    </style>
-"""
